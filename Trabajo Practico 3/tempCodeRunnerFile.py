64f74be01a47b06a009072e7cf1badadca4d7453
NahuelBerthet <nahuelberthet3@gmail.com>
2024-06-11 22:10:34 -0300

    Entrega Parcial

--- Trabajo Practico 3/tempCodeRunnerFile.py
+++ Trabajo Practico 3/tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-for i in range(len(super_heroes)):
-    if 'traje' in super_heroes[i]['biografia'] or 'armadura' in super_heroes[i]['biografia']:
-        print(i, super_heroes[i]['nombre'], super_heroes[i]['biografia'])
+print(entrenadores_con_alto_porcentaje)
